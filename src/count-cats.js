5fe2e2d47fa518632a8b3243f527e80fc4f1aa84
homchenko <posharkow@mail.ru>
2021-03-07 14:01:46 +0300

    feat: add count cats & dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,17 +2,17 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(array) {
   let countCats = 0;
-  let earsCats = "^^";
-   for (let i = 0; i < array.length; i++) {
-     for (let j = 0; j < array.length; j) {
-       if(array[i][j] == earsCats){
-         countCats++;
+    let earsCats = "^^";
+     for (let i = 0; i < array.length; i++) {
+       for (let j = 0; j < array[i].length; j++) {
+         if(array[i][j] == earsCats){
+           countCats++;
+         }
        }
      }
-   }
-   if(earsCats > 0){
-     return earsCats;
-   } else {
-     return 0;
-   }
+     if(countCats > 0){
+       return countCats;
+     } else {
+       return 0;
+     }
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,11 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  let arr = [];
+  if(Array.isArray(members)){
+    let arr = [];
   for (let i = 0; i < members.length; i++) {
     if(typeof(members[i]) == "string"){
-      arr.push(members[i].charAt(0));
+      arr.push(members[i].trim().charAt(0).toUpperCase());
     } else continue;
   }
-  return arr.sort().join('').toUpperCase();
+  return arr.sort().join('');
+} else return false;
 };
