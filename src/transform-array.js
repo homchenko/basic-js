cc0810fa3761a1f9b611c53cae6bf1f264fb0eee
homchenko <posharkow@mail.ru>
2021-03-07 20:32:30 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,24 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  let discardNext = '--discard-next';
-  let discardPrev = '--discard-prev';
-  let doubleNext = '--double-next';
-  let doublePrev = '--double-prev';
+  let discardNext = "--discard-next";
+  let discardPrev = "--discard-prev";
+  let doubleNext = "--double-next";
+  let doublePrev = "--double-prev";
   let newArr = [];
 
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] == discardNext){
-      arr.splise(arr[i+1], 1);
-    } else if(arr[i] == discardPrev){
-      arr.splise(arr[i-1], 1);
-    } else if(arr[i] == doubleNext){
-      newArr.push(arr[i+1]);
-    } else if(arr[i] == doublePrev){
-      newArr.push(arr[i-1]);
-    } else {
-      newArr.push(arr[i]);
+  if (Array.isArray(arr)) 
+  try {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == discardNext) {
+        i+=2;
+      } else if (arr[i] == discardPrev) {
+        newArr.pop();
+      } else if (arr[i] == doubleNext && i == arr.length-1){
+        break;
+      } else if (arr[i] == doubleNext) {
+        newArr.push(arr[i + 1]);
+      } else if (arr[i] == doublePrev && i != 0) {
+        newArr.push(arr[i - 1]);
+      } else if(arr[i] == doublePrev && i == 0){
+        continue;
+      } else {
+        newArr.push(arr[i]);
+      }
     }
+    return newArr;
+  } catch {
+    //return err.message;
   }
-  return newArr;
 };
