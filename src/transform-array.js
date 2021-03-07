6b32d548fdf3dcfa8c4f375df28d8e899f13f247
homchenko <posharkow@mail.ru>
2021-03-07 20:38:50 +0300

    fix: cange transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -7,27 +7,24 @@ module.exports = function transform(arr) {
   let doublePrev = "--double-prev";
   let newArr = [];
 
-  if (Array.isArray(arr)) 
-  try {
+  //if (Array.isArray(arr)) {
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] == discardNext) {
-        i+=2;
+        i += 2;
       } else if (arr[i] == discardPrev) {
         newArr.pop();
-      } else if (arr[i] == doubleNext && i == arr.length-1){
+      } else if (arr[i] == doubleNext && i == arr.length - 1) {
         break;
       } else if (arr[i] == doubleNext) {
         newArr.push(arr[i + 1]);
       } else if (arr[i] == doublePrev && i != 0) {
         newArr.push(arr[i - 1]);
-      } else if(arr[i] == doublePrev && i == 0){
+      } else if (arr[i] == doublePrev && i == 0) {
         continue;
       } else {
         newArr.push(arr[i]);
       }
     }
     return newArr;
-  } catch {
-    //return err.message;
-  }
+  
 };
